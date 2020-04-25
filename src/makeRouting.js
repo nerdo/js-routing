609c156ddbc652add3ab962c7dfcd6326814e8ec
Dannel Albert <cuebix@gmail.com>
2020-04-25 16:37:52 -0400

    + applyRouting() test scaffolding

--- src/makeRouting.js
+++ src/makeRouting.js
@@ -1,4 +1,6 @@
 export const makeRouting = () => ({
-  applyRouting() {},
+  applyRouting() {
+    return null
+  },
   addInterceptor() {}
 })
--- src/urlRouting.test.js
+++ src/urlRouting.test.js
@@ -5,4 +5,18 @@ describe('urlRouting', () => {
     expect(urlRouting).toBeDefined()
     expect(typeof urlRouting).toBe('object')
   })
+
+  describe('applyRouting()', () => {
+    const { applyRouting } = urlRouting
+
+    it('should return null when no routes are provided', () => {
+      const returnValue = applyRouting()
+      expect(returnValue).toBeNull()
+    })
+
+    it('should return null when no routes match', () => {
+      const returnValue = applyRouting([])
+      expect(returnValue).toBeNull()
+    })
+  })
 })
