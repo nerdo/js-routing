fdace5a054ab1255db690366720cf97516e48950
Dannel Albert <cuebix@gmail.com>
2020-04-26 13:27:31 -0400

    ^ makeUrlNavigationTarget

--- src/makeUrlNavigationTarget.js
+++ src/makeUrlNavigationTarget.js
@@ -1,3 +1,24 @@
-export const makeUrlNavigationTarget = input => ({
-  to: input
-})
+export const makeUrlNavigationTarget = input => {
+  const anchorIndex = input.indexOf('#')
+  const queryIndex = input.indexOf('?')
+  const toMaxIndex = anchorIndex >= 0 ? anchorIndex : (queryIndex >= 0 ? queryIndex : void 0)
+  const to = input.substr(0, toMaxIndex)
+  const params = queryIndex < 0
+    ? void 0
+    : input
+      .substr(queryIndex + 1)
+      .split('&')
+      .filter(string => string !== '')
+      .map(string => {
+        const pair = string.indexOf('=') >= 0 ? string.split('=') : [string, '']
+        return [pair[0], decodeURIComponent(pair[1])]
+      })
+      .reduce(
+        (obj, pair) => ({
+          ...obj,
+          [pair[0]]: pair[1]
+        }),
+        {}
+      )
+  return { to, params }
+}
--- src/makeUrlNavigationTarget.test.js
+++ src/makeUrlNavigationTarget.test.js
@@ -5,4 +5,44 @@ describe('makeUrlNavigationTarget()', () => {
     expect(makeUrlNavigationTarget).toBeDefined()
     expect(typeof makeUrlNavigationTarget).toBe('function')
   })
+
+  describe('return value properties', () => {
+    describe('to', () => {
+      it('should be the path minus the anchor and query string', () => {
+        const a = makeUrlNavigationTarget('/a/b/c')
+        expect(a.to).toBe('/a/b/c')
+
+        const b = makeUrlNavigationTarget('/a/b/c#anchor?query&string')
+        expect(b.to).toBe('/a/b/c')
+
+        const c = makeUrlNavigationTarget('/a/b/c?query&string')
+        expect(c.to).toBe('/a/b/c')
+      })
+    })
+
+    describe('params', () => {
+      it('should be undefined with no query string', () => {
+        const a = makeUrlNavigationTarget('/a/b/c')
+        expect(a.params).not.toBeDefined()
+      })
+
+      it('should be defined with a query string delimeter', () => {
+        const b = makeUrlNavigationTarget('/a/b/c?')
+        expect(b.params).toBeDefined()
+
+        const c = makeUrlNavigationTarget('?')
+        expect(c.params).toBeDefined()
+      })
+
+      it('should parse the query string into a plain object', () => {
+        const { params } = makeUrlNavigationTarget('/a/b/c?a&b=1&&c=hello&d=d%3D%23decode%20this%26please%3F')
+
+        expect(Object.keys(params).length).toBe(4)
+        expect(params.a).toBe('')
+        expect(params.b).toBe('1')
+        expect(params.c).toBe('hello')
+        expect(params.d).toBe('d=#decode this&please?')
+      })
+    })
+  })
 })
