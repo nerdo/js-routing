4cfda875927606118cb2d36bf1a65c63c141c92e
Dannel Albert <cuebix@gmail.com>
2020-05-19 16:38:44 -0400

    * exporting library parts

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+import * as exports from './index'
+
+describe('exports', () => {
+  it('should export the expected code', () => {
+    expect(exports.getExpandedRoutes).toBeDefined()
+    expect(exports.getParentPath).toBeDefined()
+    expect(exports.getPathParts).toBeDefined()
+    expect(exports.getSelectedUrlRoute).toBeDefined()
+    expect(exports.getUrlParamsFromRoute).toBeDefined()
+    expect(exports.makeRouter).toBeDefined()
+    expect(exports.makeUrlNavigationTarget).toBeDefined()
+    expect(exports.makeUrlRouter).toBeDefined()
+    expect(exports.NavigationHistory).toBeDefined()
+    expect(exports.objectFromQueryString).toBeDefined()
+    expect(exports.RoutingError).toBeDefined()
+  })
+})
