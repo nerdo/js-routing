98c18f3a61882cbd504943016c18e1fc46472728
Dannel Albert <cuebix@gmail.com>
2021-01-18 15:32:23 -0500

    feat: navigating when the popstate event is triggered

--- src/makeUrlPopStateHandler.ts
+++ src/makeUrlPopStateHandler.ts
@@ -1,5 +1,10 @@
-import { PopStateHandler, Router } from './interfaces';
+import { NavigationTarget, PopStateHandler, Router } from './interfaces';
 
 export const makeUrlPopStateHandler = (router: Router): PopStateHandler => {
-  return () => {}
+  return (event: PopStateEvent) => {
+    if (typeof event.state?.id !== 'undefined') {
+      const target = event.state as NavigationTarget
+      router.navigate(target.id, false, target.params, target.state)
+    }
+  }
 }
