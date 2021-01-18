2a01460088395699e84edd0bb73087fd63072982
Dannel Albert <cuebix@gmail.com>
2021-01-18 12:35:17 -0500

    test: existence of makeUrlPopStateHandler

---
+++ src/makeUrlPopStateHandler.test.ts
@@ -0,0 +1,7 @@
+import { makeUrlPopStateHandler } from './makeUrlPopStateHandler'
+
+describe('makeUrlPopStateHandler', () => {
+  it('should be defined', () => {
+    expect(makeUrlPopStateHandler).toBeDefined()
+  })
+})
